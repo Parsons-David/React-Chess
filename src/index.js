f0177abdba8487b154dbf6e8e82d7e4e0879898b
David Parsons <david.parsons@rutgers.edu>
2017-12-30 17:15:29 -0500

    Add Base Pieces

--- src/index.js
+++ src/index.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import bb from './img/bb.png';
+import bk from './img/bk.png';
+import bn from './img/bn.png';
+import bp from './img/bp.png';
+import bq from './img/bq.png';
+import br from './img/br.png';
+import wb from './img/wb.png';
+import wk from './img/wk.png';
+import wn from './img/wn.png';
+import wp from './img/wp.png';
+import wq from './img/wq.png';
+import wr from './img/wr.png';
 
 function Square(props) {
   let classes = "Square";
   classes += ( props.isMain ? " Square-main" : " Square-alt" );
   return (
     <button className={classes}>
+      <img src={props.piece} alt="" className="piece"/>
     </button>
   );
 }
 
 class Board extends React.Component{
 
-  renderSquare(isMain){
-    return <Square isMain={isMain}/>
+  renderSquare(props){
+    return <Square piece={props.piece} isMain={props.isMain}/>
   }
 
   render(){
+    const pieces = [
+      [br, bb, bn, bk, bq, bn, bb, br],
+      Array(8).fill(bp),
+      Array(8).fill(null),
+      Array(8).fill(null),
+      Array(8).fill(null),
+      Array(8).fill(null),
+      Array(8).fill(wp),
+      [wr, wb, wn, wk, wq, wn, wb, wr],
+    ];
     let board = [];
     var isMain = true;
     for(var i = 0; i < 8; i++){
       let row = [];
       for(var j = 0; j < 8; j++){
-        row.push(this.renderSquare(isMain));
+        row.push(this.renderSquare({
+          isMain : isMain,
+          piece : pieces[i][j],
+        }));
         isMain = !isMain;
       }
       isMain = !isMain;
